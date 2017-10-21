99f42d93a17ec5846c06b7ab35047ba6c4b61277
Niall O'Connor <niall.oconnor@teamaol.com>
2017-10-21 19:13:25 +0100

    Wiring up annotation and rule

--- src/main/java/com/noconnor/junitperf/JunitPerfRule.java
+++ src/main/java/com/noconnor/junitperf/JunitPerfRule.java
@@ -4,18 +4,36 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import com.noconnor.junitperf.annotations.JUnitPerfTest;
+import com.noconnor.junitperf.statements.PerformanceEvaluationStatement;
+import com.noconnor.junitperf.statements.PerformanceEvaluationStatement.PerformanceEvaluationStatementBuilder;
 
-import static com.noconnor.junitperf.statements.PerformanceEvaluationStatement.perfEvalBuilder;
 import static java.util.Objects.nonNull;
 
 public class JunitPerfRule implements TestRule {
+
+  private PerformanceEvaluationStatementBuilder perEvalBuilder;
+
+  @SuppressWarnings("WeakerAccess")
+  public JunitPerfRule() {
+    this(PerformanceEvaluationStatement.perfEvalBuilder());
+  }
+
+  // Test only
+  JunitPerfRule(PerformanceEvaluationStatementBuilder perEvalBuilder) {
+    this.perEvalBuilder = perEvalBuilder;
+  }
 
   @Override
   public Statement apply(Statement base, Description description) {
     Statement activeStatement = base;
     JUnitPerfTest perfTestAnnotation = description.getAnnotation(JUnitPerfTest.class);
     if (nonNull(perfTestAnnotation)) {
-      activeStatement = perfEvalBuilder().baseStatement(base).build();
+      activeStatement = perEvalBuilder.baseStatement(base)
+        .rateLimitExecutionsPerSecond(perfTestAnnotation.rateLimit())
+        .warmUpPeriodMs(perfTestAnnotation.warmUp())
+        .threadCount(perfTestAnnotation.threads())
+        .testDurationMs(perfTestAnnotation.duration())
+        .build();
     }
     return activeStatement;
   }
