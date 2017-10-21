f3adbfc1b39878572418cb57f37fc593651196c4
noconnor <noconnorie@protonmail.com>
2017-10-21 17:42:58 +0100

    Adding basic Rule and Statement

--- src/main/java/com/noconnor/junitperf/JunitPerfRule.java
+++ src/main/java/com/noconnor/junitperf/JunitPerfRule.java
@@ -1,8 +1,20 @@
 package com.noconnor.junitperf;
 
-import org.junit.Before;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
-public class JunitPerfRule {
+public class JunitPerfRule implements TestRule {
 
-
+  @Override
+  public Statement apply(Statement base, Description description) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        for(int i = 0; i < 10; i++) {
+          base.evaluate();
+        }
+      }
+    };
+  }
 }
--- src/test/java/com/noconnor/junitperf/JunitPerfRuleTest.java
+++ src/test/java/com/noconnor/junitperf/JunitPerfRuleTest.java
@@ -1,6 +1,16 @@
 package com.noconnor.junitperf;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 public class JunitPerfRuleTest extends BaseTest {
 
+  @Rule
+  public JunitPerfRule perfRule = new JunitPerfRule();
+
+  @Test
+  public void whenJunitPerfRuleIsPresent_thenTestCasesShouldBeExecutedMultipleTimes() {
+    System.out.println("Hello World!");
+  }
 
 }
