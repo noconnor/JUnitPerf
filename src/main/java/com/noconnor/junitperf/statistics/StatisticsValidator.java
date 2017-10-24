aefcb478921f9bf14e6ef160d1d80bfabb25c189
Niall O'Connor <niall.oconnor@teamaol.com>
2017-10-24 21:29:56 +0100

    Adding validation logic

--- src/main/java/com/noconnor/junitperf/statistics/StatisticsValidator.java
+++ src/main/java/com/noconnor/junitperf/statistics/StatisticsValidator.java
@@ -1,26 +1,42 @@
 package com.noconnor.junitperf.statistics;
 
 import lombok.Builder;
+import lombok.Value;
 
 import java.util.Map;
 
-import static com.google.common.collect.Maps.newHashMap;
+import static com.noconnor.junitperf.statistics.utils.StatisticsUtils.calculatePercentageError;
+import static com.noconnor.junitperf.statistics.utils.StatisticsUtils.calculateThroughputPerSecond;
 
 public class StatisticsValidator {
 
   private final int expectedThroughput;
   private final float allowedErrorsRate;
-  private final Map<Integer, Float> expectedPercentiles;
+  private final String percentiles;
+  private final int durationMs;
 
   @Builder
-  public StatisticsValidator(int expectedThroughput, float allowedErrorsRate, String percentiles) {
+  public StatisticsValidator(int expectedThroughput, float allowedErrorsRate, String percentiles, int durationMs) {
     this.expectedThroughput = expectedThroughput;
     this.allowedErrorsRate = allowedErrorsRate;
-    this.expectedPercentiles = newHashMap();
+    this.percentiles = percentiles;
+    this.durationMs = durationMs;
+  }
+  
+  public ValidationResult validate(Statistics statistics) {
+    return ValidationResult.builder()
+      .isThroughputAchieved(calculateThroughputPerSecond(statistics, durationMs) >= expectedThroughput)
+      .isErrorThresholdAchieved(calculatePercentageError(statistics) <= allowedErrorsRate)
+      .percentileResults(null)
+      .build();
   }
 
-  public void validate(Statistics statistics) {
-
+  @Value
+  @Builder
+  public static class ValidationResult {
+    boolean isThroughputAchieved;
+    boolean isErrorThresholdAchieved;
+    Map<Integer, Boolean> percentileResults;
   }
 
 }
