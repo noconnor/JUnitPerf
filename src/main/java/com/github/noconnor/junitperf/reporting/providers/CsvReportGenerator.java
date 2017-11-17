0c3bc4b3aa71c78e0bafc72c241a76015b70b830
noconnor <noconnorie@protonmail.com>
2017-11-17 20:08:18 +0000

    Updating CSV export format

--- src/main/java/com/github/noconnor/junitperf/reporting/providers/CsvReportGenerator.java
+++ src/main/java/com/github/noconnor/junitperf/reporting/providers/CsvReportGenerator.java
@@ -3,9 +3,11 @@ package com.github.noconnor.junitperf.reporting.providers;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +17,7 @@ import com.github.noconnor.junitperf.reporting.ReportGenerator;
 import com.google.common.base.Joiner;
 
 import static java.lang.System.getProperty;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 
 @Slf4j
@@ -32,26 +35,23 @@ public class CsvReportGenerator implements ReportGenerator {
   @Override
   public void generateReport(final Set<EvaluationContext> testContexts) {
 
-    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(reportPath),
-      "utf-8"))) {
-      writer.write("testName,duration,threadCount,throughput,minLatencyNs,maxLatencyNs,meanLatencyNs,percentileData");
+    try (BufferedWriter writer = newBufferedWriter()) {
+
+      String header = buildHeader();
+
+      writer.write(header);
       writer.newLine();
       testContexts.forEach(context -> {
 
-        List<String> percentileData = IntStream.range(1, 100).mapToObj(i -> {
-          float latency = context.getStatistics().getLatencyPercentile(i, TimeUnit.NANOSECONDS);
-          return String.format("%d:%.1f", i, latency);
-        }).collect(toList());
-
-        String record = String.format("%s,%d,%d,%d,%.1f,%.1f,%.1f,%s",
+        String record = String.format("%s,%d,%d,%d,%.4f,%.4f,%.4f,%s",
           context.getTestName(),
           context.getConfiguredDuration(),
           context.getConfiguredThreads(),
           context.getThroughputQps(),
-          context.getStatistics().getMinLatency(TimeUnit.NANOSECONDS),
-          context.getStatistics().getMaxLatency(TimeUnit.NANOSECONDS),
-          context.getStatistics().getMeanLatency(TimeUnit.NANOSECONDS),
-          Joiner.on(";").skipNulls().join(percentileData));
+          context.getStatistics().getMinLatency(MILLISECONDS),
+          context.getStatistics().getMaxLatency(MILLISECONDS),
+          context.getStatistics().getMeanLatency(MILLISECONDS),
+          Joiner.on(",").skipNulls().join(generateFormattedPercentileData(context)));
         try {
           writer.write(record);
           writer.newLine();
@@ -62,7 +62,6 @@ public class CsvReportGenerator implements ReportGenerator {
     } catch (Exception e) {
       throw new IllegalStateException(e);
     }
-
   }
 
   @Override
@@ -70,4 +69,35 @@ public class CsvReportGenerator implements ReportGenerator {
     return reportPath;
   }
 
+  private List<String> generateFormattedPercentileData(final EvaluationContext context) {
+    return IntStream.range(1, 101).mapToObj(i -> {
+      return String.format("%.4f", context.getStatistics().getLatencyPercentile(i, MILLISECONDS));
+    }).collect(toList());
+  }
+
+  private BufferedWriter newBufferedWriter() throws UnsupportedEncodingException, FileNotFoundException {
+    return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(reportPath), "utf-8"));
+  }
+
+  private String buildHeader() {
+    String header = "testName,duration,threadCount,throughput,minLatencyNs,maxLatencyNs,meanLatencyNs,<PERCENTILES>";
+    List<String> percentiles = IntStream.range(1, 101).mapToObj(CsvReportGenerator::ordinal).collect(toList());
+    header = header.replace("<PERCENTILES>", Joiner.on(",").join(percentiles));
+    return header;
+  }
+
+  // https://stackoverflow.com/questions/6810336/is-there-a-way-in-java-to-convert-an-integer-to-its-ordinal
+  private static String ordinal(int i) {
+    int mod100 = i % 100;
+    int mod10 = i % 10;
+    if (mod10 == 1 && mod100 != 11) {
+      return i + "st";
+    } else if (mod10 == 2 && mod100 != 12) {
+      return i + "nd";
+    } else if (mod10 == 3 && mod100 != 13) {
+      return i + "rd";
+    } else {
+      return i + "th";
+    }
+  }
 }
