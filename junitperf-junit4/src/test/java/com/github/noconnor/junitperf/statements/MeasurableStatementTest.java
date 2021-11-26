2419bf64552f2ce25aba47b65bdac3739a54331c
noconnor <noconnorie@pm.me>
2021-11-26 22:57:26 +0000

    Increasing coverage

--- junitperf-junit4/src/test/java/com/github/noconnor/junitperf/statements/MeasurableStatementTest.java
+++ junitperf-junit4/src/test/java/com/github/noconnor/junitperf/statements/MeasurableStatementTest.java
@@ -1,5 +1,6 @@
 package com.github.noconnor.junitperf.statements;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
@@ -138,6 +139,46 @@ public class MeasurableStatementTest {
         assertEquals(0, exampleTestClass.beforeExecutedCount);
         assertEquals(1, exampleTestClass.testStatementsExecutedCount);
         assertEquals(0, exampleTestClass.afterExecutedCount);
+    }
+
+    @Test
+    public void whenMeasurableStatementIsCreated_andBeforeStatementTriggersARunAfter_thenTestStatementShouldBeDecomposed() throws Throwable {
+        FrameworkMethod before = new FrameworkMethod(ExampleTestClass.class.getMethod("setup"));
+        FrameworkMethod after = new FrameworkMethod(ExampleTestClass.class.getMethod("teardown"));
+        FrameworkMethod test1 = new FrameworkMethod(ExampleTestClass.class.getMethod("test1"));
+
+        InvokeMethod invokeTest = new InvokeMethod(test1, exampleTestClass);
+        RunAfters afters = new RunAfters(invokeTest, singletonList(after), exampleTestClass);
+        RunBefores befores = new RunBefores(afters, singletonList(before), exampleTestClass);
+
+        MeasurableStatement measurableStatement = new MeasurableStatement(befores);
+
+        measurableStatement.runBefores();
+        measurableStatement.evaluate();
+        measurableStatement.runAfters();
+        assertEquals(1, exampleTestClass.beforeExecutedCount);
+        assertEquals(1, exampleTestClass.testStatementsExecutedCount);
+        assertEquals(1, exampleTestClass.afterExecutedCount);
+    }
+
+    @Test
+    public void whenMeasurableStatementIsCreated_andAfterHasNoBefore_thenStatementANdAfterShouldBeRunnable() throws Throwable {
+
+        FrameworkMethod after = new FrameworkMethod(ExampleTestClass.class.getMethod("teardown"));
+        FrameworkMethod test1 = new FrameworkMethod(ExampleTestClass.class.getMethod("test1"));
+
+        InvokeMethod invokeTest1 = new InvokeMethod(test1, exampleTestClass);
+        RunAfters afters1 = new RunAfters(invokeTest1, singletonList(after), exampleTestClass);
+
+        MeasurableStatement measurableStatement = new MeasurableStatement(afters1);
+
+        measurableStatement.runBefores();
+        measurableStatement.evaluate();
+        measurableStatement.runAfters();
+
+        assertEquals(0, exampleTestClass.beforeExecutedCount);
+        assertEquals(1, exampleTestClass.testStatementsExecutedCount);
+        assertEquals(1, exampleTestClass.afterExecutedCount);
     }
 
 
