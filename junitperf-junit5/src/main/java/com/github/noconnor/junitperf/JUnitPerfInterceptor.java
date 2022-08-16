64db2a0da754a4d6d7e221e3c69cbdbc255e501b
noconnor <niall.oconnor@yahooinc.com>
2022-08-16 15:50:07 +0100

    Adding POC implementation using junitperf core  library classes

--- junitperf-junit5/src/main/java/com/github/noconnor/junitperf/JUnitPerfInterceptor.java
+++ junitperf-junit5/src/main/java/com/github/noconnor/junitperf/JUnitPerfInterceptor.java
@@ -1,57 +1,93 @@
 package com.github.noconnor.junitperf;
 
+import com.github.noconnor.junitperf.data.EvaluationContext;
+import com.github.noconnor.junitperf.reporting.ReportGenerator;
+import com.github.noconnor.junitperf.reporting.providers.ConsoleReportGenerator;
+import com.github.noconnor.junitperf.statements.PerformanceEvaluationStatement;
+import com.github.noconnor.junitperf.statements.TestStatement;
+import com.github.noconnor.junitperf.statistics.providers.DescriptiveStatisticsCalculator;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
+
+import static java.lang.System.nanoTime;
+import static java.util.Objects.nonNull;
 
 public class JUnitPerfInterceptor implements InvocationInterceptor {
 
-    public static AtomicInteger COUNT = new AtomicInteger();
+    // TODO:
+    // * Figure out is static ACTIVE_CONTEXTS is required (shouldn't be required)
+    // * Figure out how to pass collection of "Reporter" instances when using @ExtendsWith (a configuration annotation??)
+    // * Figure out how to inject "StatisticsCollector" class when using @ExtendsWith (a configuration annotation??)
+    // * Maybe have a "default" console reporter that is used as a fallback when no reporters are configured
+
+    private static final Map<Class<?>, LinkedHashSet<EvaluationContext>> ACTIVE_CONTEXTS = new HashMap<>();
+    private final Set<ReportGenerator> reporters = new HashSet<>();
+
+    public JUnitPerfInterceptor() {
+        reporters.add(new ConsoleReportGenerator()); // TODO: remove this hardcoding
+    }
 
     @Override
     public void interceptTestMethod(Invocation<Void> invocation,
                                     ReflectiveInvocationContext<Method> invocationContext,
                                     ExtensionContext extensionContext) throws Throwable {
-        AtomicInteger exceptionCount = new AtomicInteger();
-        List<Thread> threads = new ArrayList<>();
 
-        // 1000 invocations
-        for (int k = 0; k < 10; k++) {
-            Thread t = new Thread(() -> {
-                for (int i = 0; i < 100; i++) {
-                    try {
-                        extensionContext.getRequiredTestMethod().invoke(extensionContext.getRequiredTestInstance());
-                    } catch (Exception e) {
-                        exceptionCount.incrementAndGet();
-                    }
+        JUnitPerfTest perfTestAnnotation = extensionContext.getRequiredTestMethod().getAnnotation(JUnitPerfTest.class);
+        JUnitPerfTestRequirement requirementsAnnotation = extensionContext.getRequiredTestMethod().getAnnotation(JUnitPerfTestRequirement.class);
+
+        if (nonNull(perfTestAnnotation)) {
+            EvaluationContext context = createEvaluationContext(extensionContext.getRequiredTestMethod());
+            context.loadConfiguration(perfTestAnnotation);
+            context.loadRequirements(requirementsAnnotation);
+
+            // Group test contexts by test class
+            ACTIVE_CONTEXTS.putIfAbsent(extensionContext.getTestClass().get(), new LinkedHashSet<>());
+            ACTIVE_CONTEXTS.get(extensionContext.getTestClass().get()).add(context);
+
+            // TODO: Move to outer class or move to junit-core
+            TestStatement testStatement = new TestStatement() {
+                @Override
+                public void runBefores() throws Throwable {
+                    // do nothing
                 }
-            });
-            threads.add(t);
-            t.start();
 
+                @Override
+                public void evaluate() throws Throwable {
+                    extensionContext.getRequiredTestMethod().invoke(extensionContext.getRequiredTestInstance());
+                }
+
+                @Override
+                public void runAfters() throws Throwable {
+                    // do nothing
+                }
+            };
+
+            PerformanceEvaluationStatement parallelExecution = PerformanceEvaluationStatement.builder()
+                    .baseStatement(testStatement)
+                    .statistics(new DescriptiveStatisticsCalculator()) // TODO: remove this hardcoding
+                    .context(context)
+                    .listener(complete -> updateReport(extensionContext.getRequiredTestMethod().getDeclaringClass()))
+                    .build();
+
+            parallelExecution.runParallelEvaluation();
+        } else {
+            System.out.println("No annotation, calling test once");
         }
-
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            }
-        });
-
-        try {
-            // extra invocation (so total will be 1001 invocations)
-            invocation.proceed();
-        } catch (Exception e) {
-            exceptionCount.incrementAndGet();
-        }
-
-        System.out.println("Test executed: " + COUNT.get());
+        invocation.proceed();
     }
+
+    EvaluationContext createEvaluationContext(Method method) {
+        return new EvaluationContext(method.getName(), nanoTime());
+    }
+
+    private synchronized void updateReport(Class<?> clazz) {
+        reporters.forEach(r -> {
+            r.generateReport(ACTIVE_CONTEXTS.get(clazz));
+        });
+    }
+
 }
--- junitperf-junit5/src/test/java/com/github/noconnor/junitperf/JUnitPerfInterceptorIntegrationTest.java
+++ junitperf-junit5/src/test/java/com/github/noconnor/junitperf/JUnitPerfInterceptorIntegrationTest.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(JUnitPerfInterceptor.class)
 public class JUnitPerfInterceptorIntegrationTest {
 
 
     @Test
+    @JUnitPerfTest(threads = 1, rampUpPeriodMs = 100, durationMs = 1_000)
     void someOtherTest() {
-        System.out.println("Running someOtherTest");
         assertTrue(true);
     }
 
     @Test
-    @ExtendWith(JUnitPerfInterceptor.class)
+    @JUnitPerfTest(threads = 10, rampUpPeriodMs = 100, durationMs = 1_000)
     void integrationTest() {
-        System.out.println("Running integrationTest");
-        JUnitPerfInterceptor.COUNT.incrementAndGet();
         assertTrue(true);
     }
 }
