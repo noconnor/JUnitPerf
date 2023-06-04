45ca06bafcc69652303ff2e509666032b210d9bc
noconnor <niall.oconnor@yahooinc.com>
2023-06-04 10:18:54 +0100

    Removing jtwig

--- junitperf-core/src/main/java/com/github/noconnor/junitperf/reporting/providers/utils/SimpleFormatter.java
+++ junitperf-core/src/main/java/com/github/noconnor/junitperf/reporting/providers/utils/SimpleFormatter.java
@@ -1,13 +1,11 @@
 package com.github.noconnor.junitperf.reporting.providers.utils;
 
-import java.io.IOException;
+import com.github.noconnor.junitperf.data.EvaluationContext;
+import com.github.noconnor.junitperf.reporting.providers.utils.SimpleFormatter.ContextHtmlFormat.RequiredPercentilesData;
+
 import java.io.InputStream;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,16 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import com.github.noconnor.junitperf.JUnitPerfTest;
-import com.github.noconnor.junitperf.JUnitPerfTestRequirement;
-import com.github.noconnor.junitperf.data.EvaluationContext;
-import com.github.noconnor.junitperf.reporting.providers.utils.SimpleFormatter.ContextHtmlFormat.RequiredPercentilesData;
-import com.github.noconnor.junitperf.statistics.providers.DescriptiveStatisticsCalculator;
-import lombok.Getter;
 
 public class SimpleFormatter {
 
@@ -42,7 +32,7 @@ public class SimpleFormatter {
         public static String populateTemplate(Object obj, String prefix, String template) throws IllegalAccessException {
             String temp = template;
             Field[] fields = obj.getClass().getDeclaredFields();
-            for(Field f : fields){
+            for (Field f : fields) {
                 f.setAccessible(true);
                 String target = "\\{\\{ " + prefix + "." + f.getName() + " \\}\\}";
                 Object value = f.get(obj);
@@ -56,16 +46,15 @@ public class SimpleFormatter {
             for (RequiredPercentilesData data : htmlContext.requiredPercentiles) {
                 String temp = populateTemplate(data, "context.percentiles", template);
                 result.append(temp).append("\n");
-            };
+            }
+            ;
             return result.toString();
         }
 
     }
 
-    @Getter
     public static class ContextHtmlFormat {
 
-        @Getter
         public static final class RequiredPercentilesData {
             private String percentile;
             private String percentileResultColour;
@@ -127,19 +116,19 @@ public class SimpleFormatter {
             this.testDurationFormatted = context.getTestDurationFormatted();
             this.throughputAchievedColour = context.isThroughputAchieved() ? SUCCESS_COLOUR : FAILED_COLOUR;
             this.throughputQps = number_format(context.getThroughputQps(), 0, ",");
-            this.requiredThroughput = number_format(context.getRequiredThroughput(), 0,  ",");
+            this.requiredThroughput = number_format(context.getRequiredThroughput(), 0, ",");
             this.minLatencyAchievedColour = context.isMinLatencyAchieved() ? SUCCESS_COLOUR : FAILED_COLOUR;
-            this.requiredMinLatency = (context.getRequiredMinLatency() < 0) ? "N/A" :number_format(context.getRequiredMinLatency(), 2,  "");
+            this.requiredMinLatency = (context.getRequiredMinLatency() < 0) ? "N/A" : number_format(context.getRequiredMinLatency(), 2, "");
             this.minLatency = number_format(context.getMinLatencyMs(), 2, " ");
             this.meanLatencyAchievedColour = context.isMeanLatencyAchieved() ? SUCCESS_COLOUR : FAILED_COLOUR;
             this.meanLatency = number_format(context.getMeanLatencyMs(), 2, " ");
-            this.requiredMeanLatency = (context.getRequiredMeanLatency() < 0) ? "N/A" :number_format(context.getRequiredMeanLatency(), 2, "");
+            this.requiredMeanLatency = (context.getRequiredMeanLatency() < 0) ? "N/A" : number_format(context.getRequiredMeanLatency(), 2, "");
             this.maxLatencyAchievedColour = context.isMaxLatencyAchieved() ? SUCCESS_COLOUR : FAILED_COLOUR;
             this.maxLatency = number_format(context.getMaxLatencyMs(), 2, ",");
-            this.requiredMaxLatency = (context.getRequiredMaxLatency() < 0) ? "N/A" :number_format(context.getRequiredMaxLatency(), 2,  "");
+            this.requiredMaxLatency = (context.getRequiredMaxLatency() < 0) ? "N/A" : number_format(context.getRequiredMaxLatency(), 2, "");
             this.requiredPercentiles = context.getRequiredPercentiles().entrySet()
                     .stream()
-                    .map( entry -> {
+                    .map(entry -> {
                         Integer percentile = entry.getKey();
                         Float target = entry.getValue();
                         RequiredPercentilesData data = new RequiredPercentilesData();
@@ -174,7 +163,7 @@ public class SimpleFormatter {
         expectedBlocks.add("{% PERCENTILES_BLOCK %}");
 
         try (Scanner scanner = new Scanner(templateString)) {
-            while(scanner.hasNext()) {
+            while (scanner.hasNext()) {
                 String line = scanner.nextLine();
                 String trimmed = line.trim();
 
@@ -199,112 +188,4 @@ public class SimpleFormatter {
         return contextBlocks;
     }
 
-
-    public static void main(String[] args) throws IllegalAccessException, IOException {
-
-        Map<String, StringBuilder> blocks = parseTemplateBlocks();
-
-        List<EvaluationContext> contexts = new ArrayList<>();
-        
-        for (int i = 0; i< 3; i++ ){
-            EvaluationContext context = new EvaluationContext("Dummy test " + i, System.nanoTime());
-            context.loadRequirements(newRequirements());
-            context.loadConfiguration(newConfiguration());
-            context.setStatistics(newStatistics());
-            context.runValidation();
-            contexts.add(context);
-        }
-
-        List<ContextHtmlFormat> htmlContexts = contexts.stream().map(ContextHtmlFormat::new).collect(Collectors.toList());
-
-        String root = blocks.get("root").toString();
-
-        StringBuilder overviews = new StringBuilder();
-        StringBuilder details = new StringBuilder();
-
-        for (ContextHtmlFormat c : htmlContexts) {
-
-            String overview = ReportGenerator.populateTemplate(c, blocks.get("{% OVERVIEW_BLOCK %}").toString());
-            String detail  = ReportGenerator.populateTemplate(c, blocks.get("{% DETAILED_BLOCK %}").toString());
-            String percentileData = ReportGenerator.populatePercentilesOverview(c, blocks.get("{% PERCENTILES_BLOCK %}").toString());
-
-            detail = detail.replaceAll("\\{% PERCENTILES_BLOCK %\\}", percentileData);
-
-            overviews.append(overview).append("\n");
-            details.append(detail).append("\n");
-        };
-
-        root = root.replaceAll("\\{% OVERVIEW_BLOCK %\\}", overviews.toString());
-        root = root.replaceAll("\\{% DETAILED_BLOCK %\\}", details.toString());
-
-        System.out.println(root);
-
-        Files.write(Paths.get("test.html"), root.getBytes());
-    }
-
-    private static JUnitPerfTest newConfiguration() {
-        return new JUnitPerfTest() {
-            @Override
-            public Class<? extends Annotation> annotationType() { return JUnitPerfTest.class; }
-            @Override
-            public int threads() { return 10; }
-            @Override
-            public int durationMs() { return 100; }
-            @Override
-            public int warmUpMs() { return 0; }
-            @Override
-            public int maxExecutionsPerSecond() { return 10_000; }
-            @Override
-            public int rampUpPeriodMs() { return 0; }
-        };
-    }
-
-
-    private static DescriptiveStatisticsCalculator newStatistics() {
-        DescriptiveStatisticsCalculator statistics = new DescriptiveStatisticsCalculator();
-        IntStream.range(0, 1_000).forEach( i -> {
-            statistics.addLatencyMeasurement(ThreadLocalRandom.current().nextInt(500_000, 1_000_000));
-            statistics.incrementEvaluationCount();
-        });
-        return statistics;
-    }
-
-    private static JUnitPerfTestRequirement newRequirements() {
-        return new JUnitPerfTestRequirement() {
-            @Override
-            public Class<? extends Annotation> annotationType() {
-                return JUnitPerfTestRequirement.class;
-            }
-
-            @Override
-            public String percentiles() {
-                return "98:3.3,99:32.6,100:46.9999";
-            }
-
-            @Override
-            public int executionsPerSec() {
-                return 10000;
-            }
-
-            @Override
-            public float allowedErrorPercentage() {
-                return 0.7F;
-            }
-
-            @Override
-            public float minLatency() {
-                return 0.98F;
-            }
-
-            @Override
-            public float maxLatency() {
-                return 89.0F;
-            }
-
-            @Override
-            public float meanLatency() {
-                return 0.99F;
-            }
-        };
-    }
 }
