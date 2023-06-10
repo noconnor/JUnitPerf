14fcd60d6de34e20e107b88557e36adcf66d7764
noconnor <niall.oconnor@yahooinc.com>
2023-06-10 12:45:32 +0100

    Adding support for SelectPackages

--- junit5-examples/src/test/java/com/github/noconnor/junitperf/examples/ExampleTestSuiteUsage.java
+++ junit5-examples/src/test/java/com/github/noconnor/junitperf/examples/ExampleTestSuiteUsage.java
@@ -24,6 +24,7 @@ import static java.util.Collections.singletonList;
 //
 
 @Suite
+//@SelectPackages( { "com.github.noconnor.junitperf.examples.existing" })
 @SelectClasses({
         TestClassOne.class,
         TestClassTwo.class
--- junitperf-junit5/src/main/java/com/github/noconnor/junitperf/suite/JunitPerfSuite.java
+++ junitperf-junit5/src/main/java/com/github/noconnor/junitperf/suite/JunitPerfSuite.java
@@ -3,20 +3,23 @@ package com.github.noconnor.junitperf.suite;
 import com.github.noconnor.junitperf.JUnitPerfTest;
 import com.github.noconnor.junitperf.JUnitPerfTestRequirement;
 import com.github.noconnor.junitperf.reporting.ReportGenerator;
+import org.junit.platform.commons.util.ClassFilter;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Objects.nonNull;
+
 public class JunitPerfSuite {
 
     private static final Map<Class<?>, JUnitPerfTest> suitePerfTestAnnotations = new HashMap<>();
     private static final Map<Class<?>, JUnitPerfTestRequirement> suiteRequirementTestAnnotations = new HashMap<>();
-
     private static final Map<Class<?>, List<ReportGenerator>> suiteReporters = new HashMap<>();
     
     public static void registerPerfTestSuite(Class<?> clazz) {
@@ -27,12 +30,27 @@ public class JunitPerfSuite {
         // TODO: update to use AnnotationUtils.findAnnotation()
         JUnitPerfTest perfTestAnnotation = clazz.getAnnotation(JUnitPerfTest.class);
         JUnitPerfTestRequirement requirementTestAnnotation = clazz.getAnnotation(JUnitPerfTestRequirement.class);
-        // TODO: add support for SelectPackages etc to identify suite classes 
+        // TODO: add support for SelectPackages etc to identify suite classes
         SelectClasses suiteClasses = clazz.getAnnotation(SelectClasses.class);
-        for (Class<?> suiteClazz : suiteClasses.value()) {
-            suitePerfTestAnnotations.put(suiteClazz, perfTestAnnotation);
-            suiteRequirementTestAnnotations.put(suiteClazz, requirementTestAnnotation);
-            suiteReporters.put(suiteClazz, reportGenerator);
+        SelectPackages suitePackages = clazz.getAnnotation(SelectPackages.class);
+        
+        if (nonNull(suiteClasses)) {
+            for (Class<?> suiteClazz : suiteClasses.value()) {
+                suitePerfTestAnnotations.put(suiteClazz, perfTestAnnotation);
+                suiteRequirementTestAnnotations.put(suiteClazz, requirementTestAnnotation);
+                suiteReporters.put(suiteClazz, reportGenerator);
+            }
+        }
+        
+        if (nonNull(suitePackages)) {
+            for (String packagePattern : suitePackages.value()) {
+                List<Class<?>> classes = ReflectionUtils.findAllClassesInPackage(packagePattern, ClassFilter.of(packageClazz -> true));
+                for (Class<?> suiteClazz : classes) {
+                    suitePerfTestAnnotations.put(suiteClazz, perfTestAnnotation);
+                    suiteRequirementTestAnnotations.put(suiteClazz, requirementTestAnnotation);
+                    suiteReporters.put(suiteClazz, reportGenerator);
+                }
+            }
         }
 
     }
